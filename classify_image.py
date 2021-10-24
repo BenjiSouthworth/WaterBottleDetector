65ae34e474ca328c4585c9927823f23b5d6527b9
mylesd6452 <doctormyles98@gmail.com>
2021-10-23 19:31:31 -0700

    Use Resnet and default weights to classify images. given enough time(days), would be could to use transfer learning to specialize. next taks it to add gui if time allows

--- classify_image.py
+++ classify_image.py
@@ -1,5 +1,80 @@
-from plastic_bottle_classifier import PlasticBottleClassifier as pbc
 
-if __name__ == '__main__':
-    print("hello")
-    #this file will have to use an already trained model and maybe take an image as argument
+# import the necessary packages
+from pyimagesearch import config
+from torchvision import models
+import numpy as np
+import torch
+import cv2
+
+def preprocess_image(image):
+	# swap the color channels from BGR to RGB, resize it, and scale
+	# the pixel values to [0, 1] range
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+	image = cv2.resize(image, (config.IMAGE_SIZE, config.IMAGE_SIZE))
+	image = image.astype("float32") / 255.0
+
+	# subtract ImageNet mean, divide by ImageNet standard deviation,
+	# set "channels first" ordering, and add a batch dimension
+	image -= config.MEAN
+	image /= config.STD
+	image = np.transpose(image, (2, 0, 1))
+	image = np.expand_dims(image, 0)
+
+	# return the preprocessed image
+	return image
+
+
+print("Enter photo name.type: ")
+
+source = "images/"
+inn = input()
+val = source+inn
+
+print("\nYou've entered:", val)
+
+image_input = val
+
+
+# load our the network weights from disk, flash it to the current
+# device, and set it to evaluation mode
+print("[INFO] loading {}...".format("resnet"))
+model = models.resnet50(pretrained=True).to(config.DEVICE)
+model.eval()
+
+# load the image from disk, clone it (so we can draw on it later),
+# and preprocess it
+print("[INFO] loading image...")
+image = cv2.imread(image_input)
+orig = image.copy()
+image = preprocess_image(image)
+
+# convert the preprocessed image to a torch tensor and flash it to
+# the current device
+image = torch.from_numpy(image)
+image = image.to(config.DEVICE)
+
+# load the preprocessed the ImageNet labels
+print("[INFO] loading ImageNet labels...")
+imagenetLabels = dict(enumerate(open(config.IN_LABELS)))
+
+# classify the image and extract the predictions
+print("[INFO] classifying image with '{}'...".format("resnet"))
+logits = model(image)
+probabilities = torch.nn.Softmax(dim=-1)(logits)
+sortedProba = torch.argsort(probabilities, dim=-1, descending=True)
+
+# loop over the predictions and display the rank-5 predictions and
+# corresponding probabilities to our terminal
+for (i, idx) in enumerate(sortedProba[0, :5]):
+	print("{}. {}: {:.2f}%".format
+		(i, imagenetLabels[idx.item()].strip(),
+		probabilities[0, idx.item()] * 100))
+
+# draw the top prediction on the image and display the image to
+# our screen
+(label, prob) = (imagenetLabels[probabilities.argmax().item()],
+	probabilities.max().item())
+cv2.putText(orig, "Label: {}, {:.2f}%".format(label.strip(), prob * 100),
+	(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
+cv2.imshow("Classification", orig)
+cv2.waitKey(0)
